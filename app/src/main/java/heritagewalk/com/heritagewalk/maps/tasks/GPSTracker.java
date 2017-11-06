8ad3d255008b00ff213f7f7a542cd3ed6035b075
Kerry Regan <kregan89@gmail.com>
2017-11-06 10:22:10 -0800

    Added GPS tracker but have yet to implement it.  The heritage site is now at the center of the screen when going to the SitePageActivity.

---
+++ app/src/main/java/heritagewalk/com/heritagewalk/maps/tasks/GPSTracker.java
@@ -0,0 +1,10 @@
+package heritagewalk.com.heritagewalk.maps.tasks;
+
+import LocationListener;
+
+/**
+ * Created by mini_regan on 2017-11-06.
+ */
+
+public class GPSTracker implements LocationListener {
+}
