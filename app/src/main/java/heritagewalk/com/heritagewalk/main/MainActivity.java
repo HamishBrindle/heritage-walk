7f85b00bd5f39bd3d6fbc2a563543899ce3a7671
Kerry Regan <kregan89@gmail.com>
2017-11-22 16:50:43 -0800

    commiting changes to string res

--- app/src/main/java/heritagewalk/com/heritagewalk/main/MainActivity.java
+++ app/src/main/java/heritagewalk/com/heritagewalk/main/MainActivity.java
@@ -1,10 +1,10 @@
 package heritagewalk.com.heritagewalk.main;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,19 +14,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import heritagewalk.com.heritagewalk.R;
-import heritagewalk.com.heritagewalk.auth.SignInActivity;
+import heritagewalk.com.heritagewalk.auth.LoginActivity;
 import heritagewalk.com.heritagewalk.maps.MapsActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth mAuth;
+    public static SharedPreferences SHARED_PREFERENCES;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +34,11 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Get the shared preferences for this instance (i.e. if user has logged in, etc.)
+        SHARED_PREFERENCES = this.getSharedPreferences(
+                "heritagewalk.com.heritagewalk", Context.MODE_PRIVATE
+        );
 
         mAuth = FirebaseAuth.getInstance();
         mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
@@ -61,7 +66,7 @@ public class MainActivity extends AppCompatActivity
                 startActivity(intent);
             }
         });
-        findViewById(R.id.signin_button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.logout_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
@@ -80,7 +85,7 @@ public class MainActivity extends AppCompatActivity
 
     private void updateUI(FirebaseUser user) {
         if (user == null) {
-            Intent intent = new Intent(this, SignInActivity.class);
+            Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
     }
