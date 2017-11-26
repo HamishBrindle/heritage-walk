ff2e84f0ad9195f595799f6331a9eb1ab7721735
Kerry Regan <Kregan89@gmail.com>
2017-11-25 19:16:01 -0800

    changed layout of achievements page

--- app/src/main/java/heritagewalk/com/heritagewalk/game/AchievementsActivity.java
+++ app/src/main/java/heritagewalk/com/heritagewalk/game/AchievementsActivity.java
@@ -32,15 +32,6 @@ public class AchievementsActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         ListView achievementList = findViewById(R.id.achievement_list);
         String[] achievements = getResources().getStringArray(R.array.achievement_list);
 
